82f29273455012f67caebe757038ab0fa984d5e3
Riley Fee <74221398+riley10445@users.noreply.github.com>
2026-02-07 18:27:51 -0600

    Finished PairingHeap, haven't tested it yet

--- PairingHeap.cpp
+++ PairingHeap.cpp
@@ -1,4 +1,4 @@
-#include "Graph.cpp"
+#include <stdexcept>
 
 class PairingHeapNode
 {
@@ -43,18 +43,18 @@ public:
 
     PairingHeapNode* Meld(PairingHeapNode* a, PairingHeapNode* b);
 
-    PairingHeapNode* MergePairs();
+    PairingHeapNode* MergePairs(PairingHeapNode* firstSibling);
 
-    void LinkChild();
+    void LinkChild(PairingHeapNode* parent, PairingHeapNode* child);
 
-    void CutFromParent();
+    void CutFromParent(PairingHeapNode* node);
 
-    void DecreaseKey();
+    void DecreaseKey(PairingHeapNode* node, int newKey);
 };
 
 PairingHeapNode* PairingHeap::Insert(int key, int vertexId){
     PairingHeapNode* newNode = new PairingHeapNode(key, vertexId);
-    PairingHeapNode* root = Meld(root, newNode);
+    root = Meld(root, newNode);
     nodeCount++;
     return newNode;
 }
@@ -82,7 +82,7 @@ PairingHeapNode* PairingHeap::DeleteMin(){
     oldRoot->nextSibling = nullptr;
     oldRoot->prevSibling = nullptr;
 
-    root = MergePairs();
+    root = MergePairs(childList);
     nodeCount--;
 
     if (root != nullptr)
@@ -93,8 +93,22 @@ PairingHeapNode* PairingHeap::DeleteMin(){
     return oldRoot;
 }
 
-void PairingHeap::DecreaseKey(){
+void PairingHeap::DecreaseKey(PairingHeapNode* node, int newKey)
+{
+    if (newKey >= node->priorityKey)
+    {
+        throw std::runtime_error("New key is not smaller than current key in DecreaseKey");
+    }
 
+    node->priorityKey = newKey;
+
+    if (node == root)
+    {
+        return;
+    }
+
+    CutFromParent(node);
+    root = Meld(root, node);
 }
 
 /*---------------------------
@@ -104,17 +118,96 @@ HELPERS
 ---------------------------*/
 
 PairingHeapNode* PairingHeap::Meld(PairingHeapNode* a, PairingHeapNode* b){
+    if (a == nullptr)
+    {
+        return b;
+    }
+    if (b == nullptr)
+    {
+        return a;
+    }
 
+    if (a->priorityKey <= b->priorityKey)
+    {
+        LinkChild(a, b);
+        return a;
+    }
+    else
+    {
+        LinkChild(b, a);
+        return b;
+    }
 }
 
-void PairingHeap::LinkChild(){
-
+void PairingHeap::LinkChild(PairingHeapNode* parent, PairingHeapNode* child){
+    child->parent = parent;
+    child->prevSibling = nullptr;
+    child->nextSibling = parent->firstChild;
+    if (parent->firstChild != nullptr)
+    {
+        parent->firstChild->prevSibling = child;
+    }
+    parent->firstChild = child;
 }
 
-void PairingHeap::CutFromParent(){
+void PairingHeap::CutFromParent(PairingHeapNode* node){
+    PairingHeapNode* parent = node->parent;
+    if (parent == nullptr)
+    {
+        return;
+    }
 
+    if (node->prevSibling != nullptr)
+    {
+        node->prevSibling->nextSibling = node->nextSibling;
+    }
+    else
+    {
+        parent->firstChild = node->nextSibling;
+    }
+
+    if (node->nextSibling != nullptr)
+    {
+        node->nextSibling->prevSibling = node->prevSibling;
+    }
+
+    node->parent = nullptr;
+    node->prevSibling = nullptr;
+    node->nextSibling = nullptr;
 }
 
-PairingHeapNode* PairingHeap::MergePairs(){
+PairingHeapNode* PairingHeap::MergePairs(PairingHeapNode* firstSibling){
+    if(firstSibling == nullptr)
+    {
+        return nullptr;
+    }
+    if(firstSibling->nextSibling == nullptr)
+    {
+        firstSibling->parent = nullptr;
+        firstSibling->prevSibling = nullptr;
+        firstSibling->nextSibling = nullptr;
+        return firstSibling;
+    }
 
+    PairingHeapNode* first = firstSibling;
+    PairingHeapNode* second = firstSibling->nextSibling;
+    PairingHeapNode* remainingSiblings = second->nextSibling;
+
+    first->parent = nullptr;
+    first->prevSibling = nullptr;
+    first->nextSibling = nullptr;
+
+    second->parent = nullptr;
+    second->prevSibling = nullptr;
+    second->nextSibling = nullptr;
+
+    if(remainingSiblings != nullptr)
+    {
+        remainingSiblings->prevSibling = nullptr;
+    }
+
+    PairingHeapNode* mergedPair = Meld(first, second);
+    PairingHeapNode* mergedRest = MergePairs(remainingSiblings);
+
+    return Meld(mergedPair, mergedRest);
 }
